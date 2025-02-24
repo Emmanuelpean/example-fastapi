dfd7087dab788d6ad415f543f455d84ce65610b9
Emmanuel Pean <emmanuel.pean@gmail.com>
2025-02-24 13:36:10 +0000

    added print

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ from app.models import (
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+print(SQLALCHEMY_DATABASE_URL)
 config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 
 # Interpret the config file for Python logging.
